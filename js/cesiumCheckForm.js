59f795c57a9d49bad430b247ac06dd1a1cdb790d
alvis-lin <alvislin43@gmail.com>
2020-07-06 11:26:43 -0400

    modified to insert geometry latlng using mouse clicks

--- js/cesiumCheckForm.js
+++ js/cesiumCheckForm.js
@@ -87,23 +87,115 @@ function dataUploaded(data) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //TESTING CODE FOR LINESTRING
+function submitLineClick() {
+  if (formLineValidation()) {
+    insertLineData();
+      alert("Thank you for your time! Your linestring have been submitted!");
+      return true;
+  } else {
+      return false;
+  }
+}
 
+
+
+// check if all the blanks in the form are filled and data is valid when user clicks submit button
+// and start to upload data to server only if all the data is valid
+// Adapted from: https://stackoverflow.com/questions/39495581/how-to-generate-alert-when-clicking-submit-button-only-when-the-html-form-is-fu
+function formLineValidation() {
+  flag = true;
+
+  // make sure the user fill in the blank, or an alert will show up
+  if (document.getElementById("line_name").value == "") {
+    alert("Please fill in Line Name!");
+    flag = false;
+  }
+
+  // if one of the blank is not filled in latitude or longitude part, an alert will show up 
+  if (document.getElementById("linestring").value == "") {
+    alert("Please click on the map to set insert linestring!");
+    flag = false;
+  }  
+
+  return flag;
+}
+
+
+
+
+
+// converting the data of the question setting form to postString to pass to the server
+// Adapted from UCL CEGE0043: Web and Mobile GIS - Apps and Programming course materials
+function insertLineData() { 
+  alert ("start line data upload"); 
+
+  // getting text values
+  var model_name = document.getElementById("line_name").value;
+  alert(model_name + " ");
+
+
+  // getting text values
+  var linestring = document.getElementById("linestring").value;
+  alert(linestring + " ");
+
+  // PostString will hold all the parameters to pass to the server
+  var postString = "model_name=" + model_name;
+  postString = postString + "&linestring="+ linestring;
+  alert(postString);
+
+  processLineData(postString);
+}
+
+
+// post the data of the question setting form to database quizquestoin table
+// Adapted from UCL CEGE0043: Web and Mobile GIS - Apps and Programming course materials
+function processLineData(postString) { 
+  var serviceUrl= "https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI + "/insertLinestring" 
+  $.ajax({ 
+    url: serviceUrl, 
+    crossDomain: true, 
+    type: "POST", 
+    success: function(data){
+      console.log(data); 
+      lineDataUploaded(data);
+    }, 
+    data: postString 
+  }); 
+}
+
+
+// processing the response from the data server and show the result on the page
+// Adapted from UCL CEGE0043: Web and Mobile GIS - Apps and Programming course materials
+function lineDataUploaded(data) { 
+  // change the DIV to show the response 
+  document.getElementById("lineDataUploadResult").innerHTML = JSON.stringify(data); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Submit polygon data into database
 function submitPolygonClick() {
   insertPolygonData();
   alert("Thank you for your time! Your polygon have been submitted!");
@@ -158,55 +250,7 @@ function polygonDataUploaded(data) {
 
 
 
-//TESTING CODE FOR LINESTRING
 
-function submitLineClick() {
-  insertLineData();
-  alert("Thank you for your time! Your linestring have been submitted!");
-}
-
-
-
-// converting the data of the question setting form to postString to pass to the server
-// Adapted from UCL CEGE0043: Web and Mobile GIS - Apps and Programming course materials
-function insertLineData() { 
-  alert ("start line data upload"); 
-
-  // getting text values
-  var model_name = document.getElementById("line_name").value;
-  alert(model_name + " ");
-
-  // PostString will hold all the parameters to pass to the server
-  var postString = "model_name=" + model_name;
-  alert(postString);
-
-  processLineData(postString);
-}
-
-
-// post the data of the question setting form to database quizquestoin table
-// Adapted from UCL CEGE0043: Web and Mobile GIS - Apps and Programming course materials
-function processLineData(postString) { 
-  var serviceUrl= "https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI + "/insertLinestring" 
-  $.ajax({ 
-    url: serviceUrl, 
-    crossDomain: true, 
-    type: "POST", 
-    success: function(data){
-      console.log(data); 
-      lineDataUploaded(data);
-    }, 
-    data: postString 
-  }); 
-}
-
-
-// processing the response from the data server and show the result on the page
-// Adapted from UCL CEGE0043: Web and Mobile GIS - Apps and Programming course materials
-function lineDataUploaded(data) { 
-  // change the DIV to show the response 
-  document.getElementById("lineDataUploadResult").innerHTML = JSON.stringify(data); 
-}
 
 
 
